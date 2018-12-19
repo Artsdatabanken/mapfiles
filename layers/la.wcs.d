25c0d178927c3107207c651edbcd376495ce38bf
Jarle Pedersen <mybigb@gmail.com>
2018-12-19 15:16:47 +0100

    added rasters and wcs

---
+++ layers/la.wcs.d
@@ -0,0 +1,147 @@
+
+
+  LAYER
+    NAME "LA-KLG-BP"
+    INCLUDE "../headers/la.wcs"
+    DATA "../data/LA_raster/KLG_BP_d.tif"
+    METADATA
+      "wcs_label"           "LA-KLG-BP"  ### required
+      "wcs_rangeset_name"   "100m"               ### required to support DescribeCoverage request
+      "wcs_rangeset_label"  "100 meters"              ### required to support DescribeCoverage request
+      "wcs_srs" "EPSG:32633 EPSG:25833"
+      "wcs_formats" "GeoTIFF" # AAIGRID XYZ" # Gtiff" # AAIGRID"
+      "wcs_resolution"             "100 100"
+     # "wcs_rangeset_nullvalue" "nan"
+     # "wcs_interval"              "-2290 2318"
+      "wcs_enable_request" "*"
+    END
+  END
+  LAYER
+    NAME "LA-KLG-IP"
+    INCLUDE "../headers/la.wcs"
+    DATA "../data/LA_raster/KLG_IP_d.tif"
+    METADATA
+      "wcs_label"           "LA-KLG-IP"  ### required
+      "wcs_rangeset_name"   "100m"               ### required to support DescribeCoverage request
+      "wcs_rangeset_label"  "100 meters"              ### required to support DescribeCoverage request
+      "wcs_srs" "EPSG:32633 EPSG:25833"
+      "wcs_formats" "GeoTIFF" # AAIGRID XYZ" # Gtiff" # AAIGRID"
+      "wcs_resolution"             "100 100"
+     # "wcs_rangeset_nullvalue" "nan"
+     # "wcs_interval"              "-2290 2318"
+      "wcs_enable_request" "*"
+    END
+  END
+  LAYER
+    NAME "LA-KLG-IYK"
+    INCLUDE "../headers/la.wcs"
+    DATA "../data/LA_raster/KLG_IYK_d.tif"
+    METADATA
+      "wcs_label"           "LA-KLG-IYK"  ### required
+      "wcs_rangeset_name"   "100m"               ### required to support DescribeCoverage request
+      "wcs_rangeset_label"  "100 meters"              ### required to support DescribeCoverage request
+      "wcs_srs" "EPSG:32633 EPSG:25833"
+      "wcs_formats" "GeoTIFF" # AAIGRID XYZ" # Gtiff" # AAIGRID"
+      "wcs_resolution"             "100 100"
+     # "wcs_rangeset_nullvalue" "nan"
+     # "wcs_interval"              "-2290 2318"
+      "wcs_enable_request" "*"
+    END
+  END
+  LAYER
+    NAME "LA-KLG-JA"
+    INCLUDE "../headers/la.wcs"
+    DATA "../data/LA_raster/KLG_JA_d.tif"
+    METADATA
+      "wcs_label"           "LA-KLG-JA"  ### required
+      "wcs_rangeset_name"   "100m"               ### required to support DescribeCoverage request
+      "wcs_rangeset_label"  "100 meters"              ### required to support DescribeCoverage request
+      "wcs_srs" "EPSG:32633 EPSG:25833"
+      "wcs_formats" "GeoTIFF" # AAIGRID XYZ" # Gtiff" # AAIGRID"
+      "wcs_resolution"             "100 100"
+     # "wcs_rangeset_nullvalue" "nan"
+     # "wcs_interval"              "-2290 2318"
+      "wcs_enable_request" "*"
+    END
+  END
+  LAYER
+    NAME "LA-KLG-RE-IA"
+    INCLUDE "../headers/la.wcs"
+    DATA "../data/LA_raster/KLG_RE_IA_d.tif"
+    METADATA
+      "wcs_label"           "LA-KLG-RE-IA"  ### required
+      "wcs_rangeset_name"   "100m"               ### required to support DescribeCoverage request
+      "wcs_rangeset_label"  "100 meters"              ### required to support DescribeCoverage request
+      "wcs_srs" "EPSG:32633 EPSG:25833"
+      "wcs_formats" "GeoTIFF" # AAIGRID XYZ" # Gtiff" # AAIGRID"
+      "wcs_resolution"             "100 100"
+     # "wcs_rangeset_nullvalue" "nan"
+     # "wcs_interval"              "-2290 2318"
+      "wcs_enable_request" "*"
+    END
+  END
+  LAYER
+    NAME "LA-KLG-RE-KF-ID"
+    INCLUDE "../headers/la.wcs"
+    DATA "../data/LA_raster/KLG_RE_KF_ID_d.tif"
+    METADATA
+      "wcs_label"           "LA-KLG-RE-KF-ID"  ### required
+      "wcs_rangeset_name"   "100m"               ### required to support DescribeCoverage request
+      "wcs_rangeset_label"  "100 meters"              ### required to support DescribeCoverage request
+      "wcs_srs" "EPSG:32633 EPSG:25833"
+      "wcs_formats" "GeoTIFF" # AAIGRID XYZ" # Gtiff" # AAIGRID"
+      "wcs_resolution"             "100 100"
+     # "wcs_rangeset_nullvalue" "nan"
+     # "wcs_interval"              "-2290 2318"
+      "wcs_enable_request" "*"
+    END
+  END
+  LAYER
+    NAME "LA-KLG-RE-KS"
+    INCLUDE "../headers/la.wcs"
+    DATA "../data/LA_raster/KLG_RE_KS_d.tif"
+    METADATA
+      "wcs_label"           "LA-KLG-RE-KS"  ### required
+      "wcs_rangeset_name"   "100m"               ### required to support DescribeCoverage request
+      "wcs_rangeset_label"  "100 meters"              ### required to support DescribeCoverage request
+      "wcs_srs" "EPSG:32633 EPSG:25833"
+      "wcs_formats" "GeoTIFF" # AAIGRID XYZ" # Gtiff" # AAIGRID"
+      "wcs_resolution"             "100 100"
+     # "wcs_rangeset_nullvalue" "nan"
+     # "wcs_interval"              "-2290 2318"
+      "wcs_enable_request" "*"
+    END
+  END
+  LAYER
+    NAME "LA-KLG-VE"
+    INCLUDE "../headers/la.wcs"
+    DATA "../data/LA_raster/KLG_VE_d.tif"
+    METADATA
+      "wcs_label"           "LA-KLG-VE"  ### required
+      "wcs_rangeset_name"   "100m"               ### required to support DescribeCoverage request
+      "wcs_rangeset_label"  "100 meters"              ### required to support DescribeCoverage request
+      "wcs_srs" "EPSG:32633 EPSG:25833"
+      "wcs_formats" "GeoTIFF" # AAIGRID XYZ" # Gtiff" # AAIGRID"
+      "wcs_resolution"             "100 100"
+     # "wcs_rangeset_nullvalue" "nan"
+     # "wcs_interval"              "-2290 2318"
+      "wcs_enable_request" "*"
+    END
+  END
+  LAYER
+    NAME "LA-KLG-VP"
+    INCLUDE "../headers/la.wcs"
+    DATA "../data/LA_raster/KLG_VP_d.tif"
+    METADATA
+      "wcs_label"           "LA-KLG-VP"  ### required
+      "wcs_rangeset_name"   "100m"               ### required to support DescribeCoverage request
+      "wcs_rangeset_label"  "100 meters"              ### required to support DescribeCoverage request
+      "wcs_srs" "EPSG:32633 EPSG:25833"
+      "wcs_formats" "GeoTIFF" # AAIGRID XYZ" # Gtiff" # AAIGRID"
+      "wcs_resolution"             "100 100"
+     # "wcs_rangeset_nullvalue" "nan"
+     # "wcs_interval"              "-2290 2318"
+      "wcs_enable_request" "*"
+    END
+  END
+
